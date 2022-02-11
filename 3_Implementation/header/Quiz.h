c241d4ce9f4f00b1c620bf13e32e156d27393051
SudheeraDasari <dasarisudheera14@gmail.com>
2022-02-11 09:16:01 +0530

    Added description to Milestone-1,#1

---
+++ 3_Implementation/header/Quiz.h
@@ -0,0 +1,27 @@
+/**
+ * @file Quiz.h
+ * @author sudheeradasari (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+//incudefiles
+   #include<stdio.h>
+   #include<conio.h>
+   #include<ctype.h>
+   #include<stdlib.h>
+   #include<string.h>
+  void show_record();
+  void reset_score();
+  void help();
+  void edit_score(float , char []);
+  int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
+     };
---
+++ 3_Implementation/test/test_Quiz.c
@@ -0,0 +1,49 @@
+#include "unity.h"
+#include <Quiz.h>
+#define PROJECT_NAME    "Quiz"
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
+     void show_record()
+     {system("cls");
+	  char name[20];
+	  float scr;
+	  FILE *f;
+	  f=fopen("score.txt","r");
+	  fscanf(f,"%s%f",&name,&scr);
+	;
+	fclose(f);
+	getch();}
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);}
+    void help()
+	{system("cls");
+    void edit_score(float score, char plnm[20])
+	{system("cls");
+	float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	  { sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%.2f",plnm,sc);
+	    fclose(f);}}
