c92c0385afbd7643d03bececbb0f6332c4730417
SudheeraDasari <dasarisudheera14@gmail.com>
2022-02-19 20:43:22 +0530

    Added Description to Milestone-1,#1

--- 3_Implementation/test/test_Quiz.c
+++ 3_Implementation/test/test_Quiz.c
@@ -1,49 +1,10 @@
+#include"Quiz.h"
+void main()
+{
+    
+    printf("\n*******Welcome to Quiz Game*******\n");
+    printf("Enter your name:");
+    gets(name);
+    mainhome();
 
-#include <Quiz.h>
-#define PROJECT_NAME    "Quiz"
-void show_record();
-void reset_score();
-void help();
-void edit_score(float , char []);
-int main()
-     {
-     int countr,r,r1,count,i,n;
-     float score;
-     char choice;
-     char playername[20];
-     void show_record()
-     {system("cls");
-	  char name[20];
-	  float scr;
-	  FILE *f;
-	  f=fopen("score.txt","r");
-	  fscanf(f,"%s%f",&name,&scr);
-	;
-	fclose(f);
-	getch();}
-
-void reset_score()
-    {system("cls");
-    float sc;
-	char nm[20];
-	FILE *f;
-	f=fopen("score.txt","r+");
-	fscanf(f,"%s%f",&nm,&sc);
-	sc=0;
-	fprintf(f,"%s,%.2f",nm,sc);
-    fclose(f);}
-    void help()
-	{system("cls");
-    void edit_score(float score, char plnm[20])
-	{system("cls");
-	float sc;
-	char nm[20];
-	FILE *f;
-	f=fopen("score.txt","r");
-	fscanf(f,"%s%f",&nm,&sc);
-	if (score>=sc)
-	  { sc=score;
-	    fclose(f);
-	    f=fopen("score.txt","w");
-	    fprintf(f,"%s\n%.2f",plnm,sc);
-	    fclose(f);}}
+}
