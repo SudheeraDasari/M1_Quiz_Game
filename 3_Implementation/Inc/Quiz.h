cc63b90ae5d33e407113a1bcfac34622dc540250
SudheeraDasari <dasarisudheera14@gmail.com>
2022-02-20 05:41:44 +0530

    Added Description to Milestone-1,#1

---
+++ 3_Implementation/Inc/Quiz.h
@@ -0,0 +1,24 @@
+/**
+ * @file Quiz.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _QUIZ_H_
+#define _QUIZ_H_
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+char name[30];
+char choose;
+
+
+void calculateScore();
+char result(char choose,char correct);
+void question();
+void menu();
